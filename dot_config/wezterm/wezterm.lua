6f39f0e5c5ffbfe6523f87d2c35e9189ae9cd9d4
Clinton Chau <clinton@peekapak.com>
2025-01-11 06:09:11 -0500

    Attempt to get LEADER to work

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,35 @@
 -- wezterm.lua
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local act = wezterm.action
 
-require("wuake").setup({
-	config = config,
-	create_unix_domain = true,
-})
+config.leader = {
+	key = "a",
+	mods = "CTRL",
+	timeout_milliseconds = 2000,
+}
+
+config.keys = {
+	{
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
+	{
+		key = "|",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "f",
+		mods = "LEADER",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	{
+		key = "t",
+		mods = "LEADER",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+}
 
 return config
