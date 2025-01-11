fe9f36d2e82d71d021c2b6d93c7d2e37b6b3de23
Clinton Chau <clinton@peekapak.com>
2025-01-11 08:55:53 -0500

    Finish basic configuration using LEADER

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,10 +1,11 @@
 -- wezterm.lua
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 local config = wezterm.config_builder()
 local act = wezterm.action
 
 config.leader = {
-	key = "a",
+	key = "\\",
 	mods = "CTRL",
 	timeout_milliseconds = 2000,
 }
@@ -15,10 +16,39 @@ config.keys = {
 		mods = "LEADER",
 		action = wezterm.action.ActivateCopyMode,
 	},
+	--- Vertical split
 	{
 		key = "|",
 		mods = "LEADER|SHIFT",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		action = act.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	-- Horizontal split
+	{
+		key = "-",
+		mods = "LEADER",
+		action = act.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		-- |
+		key = "{",
+		mods = "LEADER|SHIFT",
+		action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }),
+	},
+	{
+		key = ";",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Prev"),
+	},
+	{
+		key = "o",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Next"),
 	},
 	{
 		key = "f",
@@ -30,6 +60,112 @@ config.keys = {
 		mods = "LEADER",
 		action = act.SpawnTab("CurrentPaneDomain"),
 	},
+	{
+		key = "]",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = ",",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{
+		key = "w",
+		mods = "LEADER",
+		action = act.ShowTabNavigator,
+	},
+	-- Close tab
+	{
+		key = "&",
+		mods = "LEADER|SHIFT",
+		action = act.CloseCurrentTab({ confirm = true }),
+	},
+	-- Attach to muxer
+	{
+		key = "a",
+		mods = "LEADER",
+		action = act.AttachDomain("unix"),
+	},
+	-- Detach from muxer
+	{
+		key = "d",
+		mods = "LEADER",
+		action = act.DetachDomain({ DomainName = "unix" }),
+	},
+	-- Rename current session; analagous to command in tmux
+	{
+		key = "$",
+		mods = "LEADER|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for session",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					mux.rename_workspace(window:mux_window():get_workspace(), line)
+				end
+			end),
+		}),
+	},
+	-- Show list of workspaces
+	{
+		key = "s",
+		mods = "LEADER",
+		action = act.ShowLauncherArgs({ flags = "WORKSPACES" }),
+	},
 }
+
+-- Make it look like tabs, with better GUI controls
+config.use_fancy_tab_bar = true
+
+-- Don't let any individual tab name take too much room
+config.tab_max_width = 32
+config.colors = {
+	tab_bar = {
+		active_tab = {
+			-- I use a solarized dark theme; this gives a teal background to the active tab
+			fg_color = "#073642",
+			bg_color = "#2aa198",
+		},
+	},
+}
+
+-- Switch to the last active tab when I close a tab
+config.switch_to_last_active_tab_when_closing_tab = true
+
+config.pane_focus_follows_mouse = true
+config.scrollback_lines = 5000
+
+-- I don't really have need for padding between panes
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.unix_domains = {
+	{
+		name = "unix",
+	},
+}
+-- wezterm.on("update-right-status", function(window, pane)
+--	local leader = ""
+--	if window:leader_is_active() then
+--		leader = "LEADER"
+--	end
+--	window:set_right_status(leader)
+--end)
 
 return config
